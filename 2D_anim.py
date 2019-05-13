d63a3c7ea19f776f47f26d8d19e8f1f5b82646a2
John Tramm <john.tramm@gmail.com>
2019-05-13 12:00:14 -0500

    progress

--- 2D_anim.py
+++ 2D_anim.py
@@ -11,7 +11,7 @@ import numpy as np
 #from matplotlib.patches import Circle, Wedge, Polygon
 #from matplotlib.collections import PatchCollection
  
-data = np.loadtxt("all_paths.txt")
+data = np.loadtxt("1")
 
 x_starts  = data[:,0]
 y_starts  = data[:,1]
@@ -33,20 +33,31 @@ def map_material(i):
         return '#000000' # other?
 
 iteration = 0
+alternator = 0
+arrow = 0
 
 def plot_single_path(i):
     global iteration
-    #if iteration >= x_starts.size :
-    if iteration >= 20 :
+    global arrow
+    global alternator
+    #if iteration >= 50 :
+    print("plotting iteration ", iteration, " out of ", x_starts.size)
+    if iteration >= x_starts.size :
         return False
     x_vec = [x_starts[iteration], x_ends[iteration]]
     y_vec = [y_starts[iteration], y_ends[iteration]]
     color = map_material(materials[iteration])
     dx = x_ends[iteration] - x_starts[iteration]
     dy = y_ends[iteration] - y_starts[iteration]
-    arrow = plt.arrow(x_starts[iteration], y_starts[iteration], dx, dy, head_width=0.05, head_length=0.1, fc='k', ec='k')
-    plt.pause(0.5)
-    arrow.remove()
+    if alternator == 0 :
+        arrow = plt.arrow(x_starts[iteration], y_starts[iteration], dx, dy, head_width=0.05, head_length=0.1, fc='k', ec='k')
+        alternator = 1
+    else :
+        arrow.remove()
+        plt.plot(x_vec, y_vec, c = color )
+        iteration += 1
+        alternator = 0
+    #plt.pause(0.5)
     #plt.pause(0.1)
     #ax.clear()
     #ax.set_aspect('equal')
@@ -58,9 +69,8 @@ def plot_single_path(i):
     #ax.add_patch(circ)
     #circ=plt.Circle((0,0), radius=0.4096, color='k', fill=False)
     #ax.add_patch(circ)
-    plt.plot(x_vec, y_vec, c = color )
+    #plt.pause(0.5)
     #ax.lines[iteration-1].remove()
-    iteration += 1
 
 #xposition = [0]
 #yposition = [0]
@@ -84,6 +94,11 @@ circ=plt.Circle((0,0), radius=0.4180, color='k', fill=False)
 ax.add_patch(circ)
 circ=plt.Circle((0,0), radius=0.4096, color='k', fill=False)
 ax.add_patch(circ)
+
+Writer = animation.writers['ffmpeg']
+writer = Writer(fps=60, metadata=dict(artist='Me'), bitrate=10000)
  
-animation = animation.FuncAnimation( fig, func = plot_single_path, interval = 100 )
-plt.show()
+#animation = animation.FuncAnimation( fig, func = plot_single_path, x_starts.size, interval = 500 )
+animation = animation.FuncAnimation( fig, plot_single_path, x_starts.size*2, interval = 500 )
+#plt.show()
+animation.save('test.mp4', writer=writer, dpi=300)
